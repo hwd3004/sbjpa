d3682b49df7750f90a4c8ddf7e6d232efc5c19cd
liufeier <hwd3004@gmail.com>
2022-07-22 02:10:15 +0900

    asdsdsad

---
+++ src/main/java/com/example/sbjpa/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.example.sbjpa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+//	https://dev-pengun.tistory.com/entry/Spring-Boot-CORS-설정하기
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*");
+	}
+	
+	
+
+}
--- src/main/java/com/example/sbjpa/controller/apiController/BoardApiController.java
+++ src/main/java/com/example/sbjpa/controller/apiController/BoardApiController.java
@@ -17,9 +17,11 @@ import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -67,7 +69,7 @@ public class BoardApiController {
 	}
 
 	@GetMapping("/api/post/index")
-	public BoardResponseDto post_index(int page) {
+	public BoardResponseDto post_index(@RequestParam(required = false, defaultValue = "1") int page) {
 		try {
 			List<Board> boards;
 			int offset;
